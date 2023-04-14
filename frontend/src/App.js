8e585246dd62fe62a296c872434be55cbb38d7e6
unknown <ajfoucaultmo@mail.usf.edu>
2023-04-13 20:53:37 -0700

    01. Setting up frontend. 02. Installing TailwindCSS.

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='text-red-500 text-5xl'>Hello World!</div>
+    </div>
+  );
+}
+
+export default App;
