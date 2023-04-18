f562b4e869f4e6e93817cdd8397a6c181a2cf56f
unknown <ajfoucaultmo@mail.usf.edu>
2023-04-18 07:40:14 -0700

    03. Creating the Header component and making it responsive.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className='App'>
-      <div className='text-red-500 text-5xl'>Hello World!</div>
+      <HomePage />
     </div>
   );
 }
