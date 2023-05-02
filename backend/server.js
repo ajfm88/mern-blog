85052bd89d477bdcea2cde5e1357146cd79930ae
unknown <ajfoucaultmo@mail.usf.edu>
2023-05-01 20:25:56 -0700

    11. Connecting Node.js to Mongo DB.

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import connectDB from './config/db';
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
