bff8ec2d896585d28549db86be03201065e2ab53
unknown <ajfoucaultmo@mail.usf.edu>
2023-05-06 13:38:51 -0700

    17. Register Form.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/home/HomePage';
+import ArticleDetailPage from './pages/articleDetail/ArticleDetailPage';
+import RegisterPage from './pages/register/RegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route index path='/' element={<HomePage />} />
       <Route path='/blog/:slug' element={<ArticleDetailPage />} />
+      <Route path='/register' element={<RegisterPage />} />
     </Routes>
   );
 }
