2c26bfbae938836e1b42aa0c9a90c40bb1c31de0
unknown <ajfoucaultmo@mail.usf.edu>
2023-05-09 07:14:38 -0700

    22. Profile page. 23. Update user profile. 24. Upload profile picture. 25. Delete profile picture.

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,11 @@ app.get('/', (req, res) => {
   res.send('Server is running...');
 });
 
+app.use('/api/users', userRoutes);
+
+// static assets
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
