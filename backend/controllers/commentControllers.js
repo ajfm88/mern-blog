54e5ca5074d891f0a939417ae6205b1fad4a3d75
ajfm88 <ajfoucaultmo@mail.usf.edu>
2024-01-17 07:53:52 -0800

    53. Comments Management (Part 2).

--- backend/controllers/commentControllers.js
+++ backend/controllers/commentControllers.js
@@ -1,5 +1,5 @@
-import Comment from '../models/Comment';
-import Post from '../models/Post';
+import Comment from "../models/Comment";
+import Post from "../models/Post";
 
 const createComment = async (req, res, next) => {
   try {
@@ -8,7 +8,7 @@ const createComment = async (req, res, next) => {
     const post = await Post.findOne({ slug: slug });
 
     if (!post) {
-      const error = new Error('Post was not found');
+      const error = new Error("Post was not found");
       return next(error);
     }
 
@@ -34,7 +34,7 @@ const updateComment = async (req, res, next) => {
     const comment = await Comment.findById(req.params.commentId);
 
     if (!comment) {
-      const error = new Error('Comment was not found');
+      const error = new Error("Comment was not found");
       return next(error);
     }
 
@@ -53,16 +53,76 @@ const deleteComment = async (req, res, next) => {
     await Comment.deleteMany({ parent: comment._id });
 
     if (!comment) {
-      const error = new Error('Comment was not found');
+      const error = new Error("Comment was not found");
       return next(error);
     }
 
     return res.json({
-      message: 'Comment is deleted successfully',
+      message: "Comment is deleted successfully",
     });
   } catch (error) {
     next(error);
   }
 };
 
-export { createComment, updateComment, deleteComment };
+const getAllComments = async (req, res, next) => {
+  try {
+    const filter = req.query.searchKeyword;
+    let where = {};
+    if (filter) {
+      where.desc = { $regex: filter, $options: "i" };
+    }
+    let query = Comment.find(where);
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * pageSize;
+    const total = await Comment.find(where).countDocuments();
+    const pages = Math.ceil(total / pageSize);
+
+    res.header({
+      "x-filter": filter,
+      "x-totalcount": JSON.stringify(total),
+      "x-currentpage": JSON.stringify(page),
+      "x-pagesize": JSON.stringify(pageSize),
+      "x-totalpagecount": JSON.stringify(pages),
+    });
+
+    if (page > pages) {
+      return res.json([]);
+    }
+
+    const result = await query
+      .skip(skip)
+      .limit(pageSize)
+      .populate([
+        {
+          path: "user",
+          select: ["avatar", "name", "verified"],
+        },
+        {
+          path: "parent",
+          populate: [
+            {
+              path: "user",
+              select: ["avatar", "name"],
+            },
+          ],
+        },
+        {
+          path: "replyOnUser",
+          select: ["avatar", "name"],
+        },
+        {
+          path: "post",
+          select: ["slug", "title"],
+        },
+      ])
+      .sort({ updatedAt: "desc" });
+
+    return res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createComment, updateComment, deleteComment, getAllComments };
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,15 +1,19 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 import {
   createComment,
   deleteComment,
+  getAllComments,
   updateComment,
-} from '../controllers/commentControllers';
-import { authGuard } from '../middleware/authMiddleware';
+} from "../controllers/commentControllers";
+import { adminGuard, authGuard } from "../middleware/authMiddleware";
 
-router.post('/', authGuard, createComment);
 router
-  .route('/:commentId')
+  .route("/")
+  .post(authGuard, createComment)
+  .get(authGuard, adminGuard, getAllComments);
+router
+  .route("/:commentId")
   .put(authGuard, updateComment)
   .delete(authGuard, deleteComment);
 
